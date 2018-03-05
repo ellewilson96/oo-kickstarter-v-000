039b5368ce2d743a31110d21d013eae336786e3d
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-05 23:48:04 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,5 @@
 class Project
-  attr_accessor :name, :title, :backer
+  attr_accessor :name, :title
 
   def initialize(title)
     @title = title
@@ -8,7 +8,7 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    @backed_projects << Project.title
+    @backers << backer.title
   end
 
   def backers
