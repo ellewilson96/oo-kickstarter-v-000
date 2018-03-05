822b0c0df2d7d86a8e4f0c56df7757ed4e068c9a
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-05 23:41:18 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    @backed_projects << project.backers[0]
+    @backed_projects << project.backers.name
 end
 
   def backed_projects
